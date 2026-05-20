0c9115efa403b41d46b65114bbce2ae91b1230e4
oda <n_oda@bold.ne.jp>
2026-05-20 15:29:27 +0900

    keisanService

--- src/main/java/com/example/demo/team5/service/Team5TypingCalcService.java
+++ src/main/java/com/example/demo/team5/service/Team5TypingCalcService.java
@@ -1,5 +1,26 @@
 package com.example.demo.team5.service;
 
-public class Team5TypingCalcService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class Team5TypingCalcService {
+	//入力された文字数をdouble型にして正解率（per）を出す。
+	private double per;
+	private double perMinutes;
+	
+	public double calcPer(String correct, String total) {
+	per= correct.length() / total.length();
+	return per;
+	
+}
+
+	
+	public double calcMinutes(double time, String correct) {
+		perMinutes = correct.length() / (time/60);
+		if(time == 0) {
+			return -1;
+		}else {
+		return perMinutes;
+	}
+}
 }
