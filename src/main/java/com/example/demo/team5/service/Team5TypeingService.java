35d053279ef0640450646d54234f46d204d2b925
oda <n_oda@bold.ne.jp>
2026-05-20 16:23:58 +0900

    jikankesoku

--- src/main/java/com/example/demo/team5/service/Team5TypeingService.java
+++ src/main/java/com/example/demo/team5/service/Team5TypeingService.java
@@ -1,7 +1,17 @@
 package com.example.demo.team5.service;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class Team5TypeingService {
+//現在時刻を取得し、差を計算。ミリ秒から秒に変換するメソッド
+	public double calcTime(Instant start, Instant end) {
+		Duration duration = Duration.between(start, end);
+		double time = duration.toMillis() / 1000;
+		return time;
+	}
+	
 }
