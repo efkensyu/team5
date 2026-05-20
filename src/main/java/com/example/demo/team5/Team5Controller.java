b28658694135d3466a107b8561cdc214e9a153cd
ry_saito <ry_saito@bold.ne.jp>
2026-05-20 15:44:53 +0900

    Controller側コメント追記及びサービス一部修正

--- src/main/java/com/example/demo/team5/Team5Controller.java
+++ src/main/java/com/example/demo/team5/Team5Controller.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller	
 public class Team5Controller {
+public static double start=0,end=0;
+	
 
-
+//ログイン画面
 	//ログイン画面へ
 		@GetMapping("/Team5/login")		
 		public String index () throws Throwable{		
@@ -20,56 +22,55 @@ public class Team5Controller {
 		
 		//ログイン画面からメニュー画面へ
 		@PostMapping(value ="/Team5/menu" ,params = "next")		
-		public String send1() throws Throwable{	
-
-			
-				
+		public String send1() throws Throwable{
+			System.out.println("login to menu");
 			return "team5/Team5menu";	
-		}		
+		}
+	//メニュー画面処理
 		//メニュー画面からタイピング画面へ
 		@PostMapping(value ="/Team5/typi",params="next")		
 		public String send2(Model model) throws Throwable{
-			
+			System.out.println("menu to typing");
 			return "team5/Team5typing";	
 		}
 		//タイピング画面からメニュー画面へ
-				@PostMapping(value ="/Team5/menu",params="return")	
-				public String send02(Model model) {
+		@PostMapping(value ="/Team5/menu",params="return")	
+		public String send02() {
 					
-					return "team5/Team5menu";	
-				}
+				return "team5/Team5menu";	
+		}
 				
 				
-		
+	//メイン処理部分
+	//文字入力を受け取り、時間計測を開始する部分。
+	//セッション時間計測を開始させる。
 		//タイピング画面からタイピング実施画面へ
 		@PostMapping(value ="/Team5/uchikomi",params="clear")		
 		public String send3()throws Throwable {
-			
-			
 			return "team5/Team5uchikomi";
+		}
 		
-	
-	}
 		//タイピング実施画面からタイピング画面へ
 		@PostMapping(value ="/Team5/typi",params="return")	
 		public String send03(Model model) {
 			System.out.println("戻る");
 			return "team5/Team5typing";	
 		}
+		
 		//タイピング実施画面から結果画面へ
+		//計測終了
 		@PostMapping(value ="/Team5/result",params="next")		
 		public String send5() {
-			System.out.println("susum");
+			System.out.println("susum" + start + end);
 			
 			return "team5/Team5result";
 		
 		}
-//タイピング結果画面からメニュー画面へ
-				@PostMapping(value ="/Team5/result1",params="next")		
-				public String send4() {
-					
-					
-					return "team5/Team5menu";
+		//タイピング結果画面からメニュー画面へ
+		@PostMapping(value ="/Team5/result1",params="next")		
+		public String send4() {
+			
+			return "team5/Team5menu";
 				
 			
 			}
