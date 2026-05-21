6b0592073f314508880740c6f6e9b66f4c9cc173
suzuki.yu <yuk_suzuki@bold.ne.jp>
2026-05-21 09:58:53 +0900

    aspect add

--- src/main/java/com/example/demo/team5/Team5AoP.java
+++ src/main/java/com/example/demo/team5/Team5AoP.java
@@ -1,10 +1,31 @@
 package com.example.demo.team5;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Aspect
 @Component
 public class Team5AoP {
+	@After("annotation(* com.example.demo.team5.Team5Controller.PostMapping(..))")
+	public void AfterLog(JoinPoint jp) {
+		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
+		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
+		log.info("Postメソッドのinfoログ");
+		log.warn("Postメソッドのwarningログ");
+		log.error("Postメソッドのwarningログ");
+	}
+	@After("annotation(* com.example.demo.team5.Team5Controller.GetMapping(..))")
+	public void AfterLog1(JoinPoint jp) {
+		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
+		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
+		log.info("getメソッドのinfoログ");
+		log.warn("getメソッドのwarningログ");
+		log.error("getメソッドのwarningログ");
+	}
 
 }
