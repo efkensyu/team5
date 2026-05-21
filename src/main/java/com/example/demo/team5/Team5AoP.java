047470e3c161ef50ba39707fd94e774f549afb5f
suzuki.yu <yuk_suzuki@bold.ne.jp>
2026-05-21 10:44:47 +0900

    asp add

--- src/main/java/com/example/demo/team5/Team5AoP.java
+++ src/main/java/com/example/demo/team5/Team5AoP.java
@@ -2,6 +2,7 @@ package com.example.demo.team5;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +17,29 @@ public class Team5AoP {
 		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
 		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
 		log.info("Postメソッドのinfoログ");
-		log.warn("Postメソッドのwarningログ");
-		log.error("Postメソッドのwarningログ");
+		
+	}
+	@AfterThrowing("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+	public void AfterTGLog(JoinPoint jp) {
+		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
+		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
+		
+		log.error("Postメソッドのerrorログ");
 	}
 	@After("@annotation(org.springframework.web.bind.annotation.GetMapping)")
 	public void AfterLog1(JoinPoint jp) {
 		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
 		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
 		log.info("getメソッドのinfoログ");
-		log.warn("getメソッドのwarningログ");
-		log.error("getメソッドのwarningログ");
+		
+		
+	}
+	@AfterThrowing("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+	public void AfterTPLog1(JoinPoint jp) {
+		//System.out.println("[After]PotMappingメソッド開始:" + jp.getSignature());
+		//System.out.println("送ったデータ:" + Arrays.toString(jp.getArgs()));
+		
+		log.error("getメソッドのerrorログ");
 	}
 
 }
