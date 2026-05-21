2a4141308493e3e76d3a07a2960e43d90ac1fb39
oda <n_oda@bold.ne.jp>
2026-05-21 15:44:10 +0900

    CSS名前変更

--- src/main/java/com/example/demo/team5/repository/Team5ScoreRepository.java
+++ src/main/java/com/example/demo/team5/repository/Team5ScoreRepository.java
@@ -4,8 +4,10 @@ package com.example.demo.team5.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.team5.entity.Team5Score;
+
 
 @Repository
-public interface Team5ScoreRepository extends JpaRepository<Team5ScoreRepository, Integer> {
+public interface Team5ScoreRepository extends JpaRepository<Team5Score, Integer> {
 	
 }
