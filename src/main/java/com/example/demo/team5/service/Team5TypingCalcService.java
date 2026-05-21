b25080660aa1dfbdd172c1aa16c40a1573c9704f
ry_saito <ry_saito@bold.ne.jp>
2026-05-21 14:08:52 +0900

    typingサービス周り可読性向上のため修正

--- src/main/java/com/example/demo/team5/service/Team5TypingCalcService.java
+++ src/main/java/com/example/demo/team5/service/Team5TypingCalcService.java
@@ -8,18 +8,20 @@ public class Team5TypingCalcService{
 	
 	
 	//正解確率を出力する。
-	public double calcPer(String inputText, String totalText) {
-	double per = inputText.length() / totalText.length();
+	//引数は（正解文字数,合計文字数)
+	public double calcPer(int correct, int total) {
+	double per = correct / total;
 	return per;
 	
 }
 	//分間打文字数を出力する。
-	public double calcMinutes(double time, String correct) {
+	//引数は（時間、正解文字数）
+	public double calcMinutes(double time, int correct) {
 		//timeが0の場合、エラー値として-1を返す。
 		if(time == 0) {
 			return -1;
 		}else {
-			double perMinutes = correct.length() / (time/60);	
+			double perMinutes = correct / (time/60);	
 		return perMinutes;
 	}
 	
