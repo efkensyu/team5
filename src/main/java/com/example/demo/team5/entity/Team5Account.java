dec9909f2c0f068c633c36902f74e47d3ab429cd
ry_saito <ry_saito@bold.ne.jp>
2026-05-25 17:15:47 +0900

    ログイン及びログアウト遷移作成

--- src/main/java/com/example/demo/team5/entity/Team5Account.java
+++ src/main/java/com/example/demo/team5/entity/Team5Account.java
@@ -3,7 +3,6 @@ package com.example.demo.team5.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -15,7 +14,7 @@ import lombok.Data;
 public class Team5Account {
 	@Id
 	@NotBlank(message = "ユーザーネームを入力してください")
-	@Max(value = 30 , message = "ユーザ名は30字以下にしてください。")
+	@Size(max = 30 , message = "ユーザ名は30字以下にしてください。")
 	private String userId;
 	
 	@NotBlank(message = "パスワードを入力してください")
